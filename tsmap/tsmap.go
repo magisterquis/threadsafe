531cfa38e714733976c11bb132aee5c553e67c9e
Stuart <kd5pbo@gmail.com>
2014-08-28 05:45:10 -0400

    Works a bit better?

--- tsmap/tsmap.go
+++ tsmap/tsmap.go
@@ -4,69 +4,59 @@
 //  m := Map(string, int)
 //  m.Put("foo", 1)
 //  fmt.Printf("%v: %v\n", "foo", m.Get("foo")) /* "foo: 1" */
-package threadsafe/tsmap
+package tsmap
 
 import (
-        "errors"
-        "sync"
+	"sync"
 )
 
 /* Map implements a threadsafe map. */
-type Map {
-        sync.RWMutex
-        m map[interface{}]interface{}
-        k type /* Key type */
-        v type /* Value type */
+type Map struct {
+	sync.RWMutex
+	m map[string]interface{}
 }
 
-/* New creates a new Map, with keys of type k and values of type v. */
-func New(k, v type) *Map {
-        m := &Map{k: k, v: v}
+/* New makes a new map */
+func New() *Map {
+        m := &Map{}
+        m.m = map[string]interface{}{}
         return m
 }
 
-/* Get returns the value pointed to by k, which must be the type specified
-when the Map was created. */
-func (m *map) Get(k interface{}) (interface{}, bool, error) {
-        m.RLock()
-        defer m.RUnlock()
-        /* Typecast */
-        if v, ok := k.(m.k); !ok {
-                return nil, errors.New("Invalid type %T for key %v; "+
-                "expected %v", k, k, m.k)
-        }
-        v, ok := m[k]
-        return v, ok, nil
+
+/* Get returns the value pointed to by k */
+func (m *Map) Get(k string) (interface{}, bool) {
+	m.RLock()
+	defer m.RUnlock()
+	v, ok := m.m[k]
+	return v, ok
+
 }
 
-/* Put puts value v at key k in the Map.  Both values are type-checked before
-insertion. */
-func (m *Map) Put(k, v interface{}) error {
-        m.Lock()
-        defer m.Unlock()
-        if _, ok := k.(m.k); !ok {
-                return errors.New("Invalid type %T for key %v; expected %v",
-                k, k, m.k)
-        }
-        if _, ok := v.(m.v); !ok {
-                return errors.New("Invalid type %T for value %v; expected %v",
-                v, v, m.v)
-        }
-        m.m[k] = v
-        return nil
+/* Put puts value v at key k in the Map. */
+func (m *Map) Put(k string, v interface{}) {
+	m.Lock()
+	defer m.Unlock()
+	m.m[k] = v
+}
+
+/* PutUnique puts value v at key k only if k does not exist already, and
+returns whether the put was successful or not. */
+func (m *Map) PutUnique(k string, v interface{}) bool {
+	m.Lock()
+	defer m.Unlock()
+	if _, ok := m.m[k]; ok {
+		return false
+	}
+	m.m[k] = v
+	return true
 }
 
 /* Delete the key/value pair with key k. */
-func (m *Map)Delete(k) error {
-m.Lock()
-defer m.Unlock()
-        if _, ok := k.(m.k); !ok {
-                return errors.New("Invalid type %T for key %v; expected %v",
-                k, k, m.k)
-        }
-        delete(m, k)
-        return nil
+func (m *Map) Delete(k string) {
+	m.Lock()
+	defer m.Unlock()
+	delete(m.m, k)
 }
 
-
-
+/* TODO: Function to apply function to value */
